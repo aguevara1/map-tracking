a5be0495ea7be0e2e5966b6c5636767fa826b7e6
aguevara1 <albguevara12@gmail.com>
2021-07-23 13:53:32 -0500

    initial commit with new files

---
+++ index.js
@@ -0,0 +1,19 @@
+let express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+let clientRoutes=require("./routes/client.js");
+
+
+
+app.use(clientRoutes);
+
+
+
+let port = process.env.PORT || 8080;
+ /*if (port == null || port == "") {
+  port = 3000;
+}  */
+app.listen(port);
---
+++ public/app.js
@@ -0,0 +1,71 @@
+
+import "https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js";
+
+
+
+const start= document.querySelector("#start");
+const stop= document.querySelector("#stop");
+
+const coordinates =[
+  [13.40499, 51.520024],
+  [13.40500, 51.520025],
+  [13.40502, 51.520026],
+  [13.40503, 51.520027],
+];
+
+start.addEventListener("click", () =>{
+ //watchPosition will get position everytime browser thinks
+ //the position changed
+  navigator.geolocation.watchPosition( data => {
+    console.log("hello");
+   console.log(data);
+   console.log(coordinates);
+   coordinates.push([data.coords.longitude,data.coords.latitude]);
+   window.localStorage.setItem("coordinates", JSON.stringify(coordinates));
+
+   }, (error) => console.log(error),
+   {
+      enableHighAccuracy:true
+   }
+   
+
+ );
+
+});
+
+
+  //mapbox expects the opposite of coordinates, it wants longitude and then latitude
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxiZS1ydG8xIiwiYSI6ImNrcmR4Y2Y3dDF3bTcydWxxenNlMTI4bmQifQ.aR6NAaPodQLDgePGeVQ7hQ';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center:[13.40497, 51.520023],
+zoom: 15
+});
+
+map.on('load', function () {
+    map.addSource('route', {
+    type: 'geojson',
+    data: {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+    type: 'LineString',
+    coordinates:coordinates
+    }
+    }
+    });
+    map.addLayer({
+    id: 'route',
+    type: 'line',
+    source: 'route',
+    layout: {
+    'line-join': 'round',
+    'line-cap': 'round'
+    },
+    paint: {
+    'line-color': '#FF0000',
+    'line-width': 10
+    }
+    });
+    });
